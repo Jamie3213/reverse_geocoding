10615ef8fd20e052322e414ed90863a4f6781ec0
Jamie Hargreaves <jamiehargreaves@Jamies-MacBook-Air.local>
2021-01-23 18:02:52 +0000

    Add configuration template

---
+++ src/config_template.py
@@ -0,0 +1,13 @@
+config = {
+    'elasticsearch': {
+        'ES_URL': 'url',
+        'ES_USER': 'username',
+        'ES_PWD': 'password',
+        'ES_INDEX': 'index name'
+        'ES_BOOL_IS_LOCAL': 'True or False' 
+    }
+    'data': {
+        'CENSUS_BOUNDARIES': 'path to shapefile',
+        'CENSUS_LOOKUP': 'path to lookup csv'
+    }
+}
